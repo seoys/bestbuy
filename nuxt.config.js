e7ccb43ee6b168b859d7d3ad4bcc91f673d1407c
seoys <angelskying@gmail.com>
2017-12-24 01:24:59 +0900

    설정 변경 진행함

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
   /*
   ** Headers of the page
@@ -7,7 +9,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'bestbuy' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -24,6 +26,14 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
+    vender: ['jquery', 'materialize-css'],
+    plugins:[
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery'
+      })
+    ],
     extend (config, ctx) {
       if (ctx.dev && ctx.isClient) {
         config.module.rules.push({
@@ -34,5 +44,6 @@ module.exports = {
         })
       }
     }
-  }
+  },
+  plugins: ['~plugins/bootstrap.js']
 }
---
+++ pages/product.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title"></h1>
+      <h2 class="subtitle"></h2>
+    </div>
+  </section>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
